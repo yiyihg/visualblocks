698f031e918c9e34ee9ee45cd755cdf2dbe456fa
yiyihg <138255170+yiyihg@users.noreply.github.com>
2023-07-06 17:35:15 +0000

    small fix

--- testpy.py
+++ testpy.py
@@ -7,7 +7,7 @@ with open("pipelines_index.json", "w") as json_file:
     for root, _, files in os.walk('pipelines'):
         for file in files:
             data.append({
-                name: str(root+file)
+                'name': str(root+file)
             })
     json.dump(data, json_file, ensure_ascii=False)
 
