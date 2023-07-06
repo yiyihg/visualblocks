9c6261703d494dca246130b6a196e8f5d6299f28
yiyihg <138255170+yiyihg@users.noreply.github.com>
2023-07-06 00:10:00 +0000

    edited testpy

--- testpy.py
+++ testpy.py
@@ -1,3 +1,13 @@
 
-print("hello")
-f = open("myfile.txt", "w")
+import os
+import json
+
+with open("pipelines_index.json", "w") as json_file:
+    data = []
+    for root, _, files in os.walk('pipelines'):
+        for file in files:
+            data.append({
+                name: str(root+file)
+            })
+    json.dump(data, json_file, ensure_ascii=False)
+
